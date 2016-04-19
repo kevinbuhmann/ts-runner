34262239989d24fd6d07e55c8a5977b776a79a41
Kevin Phelps <KevinPhelps11@gmail.com>
2016-04-18 20:11:43 -0500

    lint fixes

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ function run(sourcePath, _this, _arguments, options, tempFilename) {
     let tscPath = path.join('node_modules', '.bin', 'tsc');
     try {
         childProcess.execSync(`${tscPath} ${sourcePath} --out ${tempFilename} ${options}`);
-    } catch(e) {
+    } catch (e) {
         let output = e.output.toString();
         console.error(output.substring(1, output.length - 1));
-        throw new Error('tsc compile failed')
+        throw new Error('tsc compile failed');
     }
 
-    let jsCode = fs.readFileSync(tempFilename)
+    let jsCode = fs.readFileSync(tempFilename);
     fs.unlinkSync(tempFilename);
 
     let jsWrapped = eval(`(function (exports, require, module, __filename, __dirname) {${jsCode}})`);
